4bc5c27fee7bb21017766f1d846176564afdff28
iwase_mio <mio.disney110@gmail.com>
2024-03-08 00:08:36 +0900

    variables追加

---
+++ src/variables/fontfamily.js
@@ -0,0 +1,6 @@
+const FONTFAMILY = {
+  NOTO_SANS: "'Noto Sans JP', sans-serif",
+  NOTO_SERIF: "'Noto Serif JP', sans-serif",
+  TIMES: "'Times New Roman', Georgia, serif",
+};
+export default FONTFAMILY;

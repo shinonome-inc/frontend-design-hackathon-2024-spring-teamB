abc6c167d073ccb508b626bcacfeed85955136c2
Yuseidosukoiya <y.fukushima.23.5.964@4nonome.com>
2024-03-10 21:52:06 +0900

    言語対応ボタンの作成

---
+++ src/component/Molecules/Languages/index.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import COLORS from "../../../variables/colors";
+import FONTFAMILY from "../../../variables/fontfamily";
+
+//画像が表示されない、pngでもダメだった
+import grayarrow from "../../../../public/assets/Chevron-d.svg";
+import globe from "../../../../public/assets/ei-language1.svg";
+
+export const Languages = () => {
+  const [isSelecting, setIsSelecting] = useState(false);
+
+  //言語対応のしるしとして一応作りましたが意味はないです
+  const [languages, setLanguages] = useState("ja");
+  const onClick = () => {
+    setIsSelecting(true);
+  };
+
+  const isEnglish = () => {
+    setLanguages("en");
+    setIsSelecting(false);
+  };
+
+  const isJapanese = () => {
+    setLanguages("ja");
+    setIsSelecting(false);
+  };
+
+  return (
+    <StyledView>
+      <StyledBox>
+        <img src={globe} alt="globe image" />
+        <StyledSelectingButton onClick={onClick}>
+          <StyledText>言語を選択</StyledText>
+          <img src={grayarrow} alt="gray arrow image" />
+        </StyledSelectingButton>
+      </StyledBox>
+      {isSelecting && (
+        <StyledScrollBox>
+          <StyledButtonEn onClick={isEnglish}>English</StyledButtonEn>
+          <StyledButtonJa onClick={isJapanese}>日本語</StyledButtonJa>
+        </StyledScrollBox>
+      )}
+    </StyledView>
+  );
+};
+
+export default Languages;
+
+const StyledView = styled.div`
+  width: 200px;
+`;
+
+const StyledBox = styled.div`
+  display: flex;
+  padding: 4px 8px;
+  border-radius: 2px;
+`;
+
+const StyledSelectingButton = styled.button`
+  display: flex;
+  margin-left: 12px;
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+`;
+
+const StyledText = styled.div`
+  margin-right: 12px;
+  font-family: ${FONTFAMILY.NOTO_SANS};
+  font-size: 12px;
+`;
+
+//色々試したのですがscrollbarが反映されない、おそらく要素が少なすぎるから？→今回はなくてもいいかも
+const StyledScrollBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 4px 8px;
+  border-radius: 2px;
+  margin-top: 2px;
+  overflow-y: scroll;
+  :-webkit-scrollbar {
+    width: 5px;
+    border-radius: 4px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: ${COLORS.Light_Gray};
+  }
+  ::-webkit-scrollbar-button {
+    display: none;
+  }
+  ::-webkit-scrollbar-button-start {
+    display: none;
+  }
+  ::-webkit-scrollbar-button-end {
+    display: none;
+  }
+  ::-webkit-scrollbar-thumb {
+    height: 36.61px;
+    border-radius: 4px;
+    background-color: ${COLORS.Dark_Gray};
+  }
+`;
+
+const StyledButtonEn = styled.button`
+  width: 100%;
+  padding: 6px 4px;
+  font-family: ${FONTFAMILY.TIMES};
+  color: ${COLORS.Dark_Gray};
+  cursor: pointer;
+  font-size: 12px;
+  border: none;
+  background-color: transparent;
+  text-align: left;
+`;
+
+const StyledButtonJa = styled.button`
+  width: 100%;
+  padding: 6px 4px;
+  font-family: ${FONTFAMILY.NOTO_SANS};
+  color: ${COLORS.Dark_Gray};
+  cursor: pointer;
+  font-size: 12px;
+  border: none;
+  background-color: transparent;
+  text-align: left;
+`;

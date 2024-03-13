ff8abafab9c5a15542328f2a2bda29051abb8e19
masato626 <m.takiguchi.22.7.561@4nonome.com>
2024-03-13 17:30:35 +0900

    Update index.jsx

--- src/pages/question1/index.jsx
+++ src/pages/question1/index.jsx
@@ -1,99 +1,17 @@
-import styled from "styled-components";
-import Templates from "../component/Templates";
-import React, { useEffect, useState } from "react";
-import FONTFAMILY from "../../variables/fontfamily";
-import { Question } from "../component/Molecules/Question";
-import Button from "../component/Atoms/Button";
 import { paths } from '../../paths';
 import { useRouter } from "next/router";
 
-const QuestionPage = ({ language }) => {
-  const [isMd, setIsMd] = useState(false);
-  const leftImage = isMd ? "/assets/leftmd.png" : "/assets/lgleft.png";
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMd(window.innerWidth <= 1064);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const questionText = language === "english" ? "Question 1" : "質問1";
-  const backButton =
-    language === "english" ? "Back to Top" : "トップページへ戻る";
-    const router = useRouter();
-    const onClick1 = () => {
-      router.push(paths.question21);
-    }
-    const onClick2 = () => {
-      router.push(paths.question22);
-    }
+const Question1Page = () => {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(paths.question2);
+  }
   return (
-    <Templates>
-      <TextWrapper>{questionText}</TextWrapper>
-      <Content>
-        <LeftImage src={leftImage} alt="Left Image" />
-        <QuestionWrapper>
-          <Question questionText="aaaaaaaaaaaaaaaaaaaaaaaaa" />
-        </QuestionWrapper>
-      </Content>
-      <YellowButtonWrapper>
-        <Button children="選択肢1" variant="yellow" onClick={onClick1}/>
-        <Button children="選択肢2" variant="yellow" onClick={onClick2}/>
-      </YellowButtonWrapper>
-      <ButtonWrapper>
-        <Button children={backButton} variant="default" />
-      </ButtonWrapper>
-    </Templates>
+    <div>
+      <h1>question1</h1>
+      <button onClick={onClick}>はい</button>
+    </div>
   );
 };
-export default QuestionPage;
 
-const Content = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-top: 20px;
-`;
-
-const LeftImage = styled.img`
-  max-width: 100%;
-  height: auto;
-  max-height: 540px;
-
-  @media (max-width: 1064px) {
-    max-width: 334px;
-    max-height: 223px;
-  }
-`;
-
-const QuestionWrapper = styled.div`
-  margin-left: 20px;
-`;
-
-const TextWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-family: ${FONTFAMILY.NOTO_SERIF};
-  font-size: 96px;
-  font-weight: 700;
-  padding: 20px;
-`;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
-  padding: 0 0 40px 0;
-`;
-const YellowButtonWrapper = styled.div`
-  display: flex;
-  gap: 16px;
-  justify-content: center;
-`;
+export default Question1Page;

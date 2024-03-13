5148d6d677d92f3dbb0f7bae9702739972b70141
Yuseidosukoiya <y.fukushima.23.5.964@4nonome.com>
2024-03-13 13:27:09 +0900

    exampleに反映

--- src/component/Atoms/Example/index.jsx
+++ src/component/Atoms/Example/index.jsx
@@ -1,6 +1,32 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
 import React from "react";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
+
+import translation_en from "../../../i18n/en.json";
+import translation_ja from "../../../i18n/ja.json";
+
+const resources = {
+  ja: {
+    translation: translation_ja,
+  },
+  en: {
+    translation: translation_en,
+  },
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "ja",
+    debug: true,
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
 
 export const Example = () => {
   const { t } = useTranslation();
